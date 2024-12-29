0171f6023b4264a0351156cca501c43fc64033be
haadirakhangi <haadirakhangi@gmail.com>
2024-12-29 19:55:06 +0530

    quiz generator added

--- server-side/core/quiz_generator.py
+++ server-side/core/quiz_generator.py
@@ -3,5 +3,5 @@ from api.gemini_client import GeminiProvider
 class QuizGenerator:
     def __init__(self):
         self.gemini_client = GeminiProvider()
-
-    
+    def generate_quiz_from_resume(resume_text : str):
+        pass
